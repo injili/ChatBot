dc7674b6d01965c20414f3b40341b3e46737370e
injili <nyarekigospel@gmail.com>
2023-07-25 12:46:57 +0300

    main file bug rectification

--- main_file.py
+++ main_file.py
@@ -15,7 +15,7 @@ def message_probability(user_message, recognised_words,
     message_certainty = 0
     req_words = True
 
-    for words in user_message:
+    for word in user_message:
         if word in recognised_words:
             message_certainty += 1
 
@@ -39,6 +39,7 @@ def check_messages(message):
 
     with open('responses.json') as f:
         responses_data = json.load(f)
+        responses_list = responses_data.get('responses', [])
 
     def response(bot_response, list_of_words, single_response=False,
                  required_words=[]):
@@ -49,13 +50,19 @@ def check_messages(message):
                                   required_words)
         highest_probability_list[bot_response] = var
 
-    for response_key, response_data in responses_data['responses']:
+    for response_data in responses_list:
         response(response_data['response'], response_data['keywords'],
                  response_data.get('single_response', False),
                  response_data.get('required_words', []))
 
-    best_match = max(highest_probability_list, key=highest_probability_list.get)
-    return long.unknown() if highest_probability_list[best_match] < 1 else best_match
+    best_match = max(highest_probability_list,
+                     key=highest_probability_list.get)
+    return (
+            long.unknown()
+            if highest_probability_list[best_match] < 1
+            else best_match
+            )
+
 
 def get_response(user_input):
     """
@@ -63,6 +70,7 @@ def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
     response = check_messages(split_message)
     return response
+
 
 while True:
     print('Bot: ' + get_response(input('You: ')))
