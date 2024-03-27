c82d8044344bde44f90390288ac6b70e20157bac
injili <nyarekigospel@gmail.com>
2024-03-27 15:40:48 +0300

    done done

--- app.py
+++ app.py
@@ -29,5 +29,10 @@ def get_bot_response():
     response = get_completion(userText)
     return response
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
